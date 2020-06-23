ebfc17a7492b21f40b00968335a6ff60900a23ad
Jelani Woods <jelani@firstdraft.com>
2020-06-23 17:32:09 -0500

    Updates

--- config.ru
+++ config.ru
@@ -2,17 +2,5 @@
 
 require_relative 'config/environment'
 
-# This file is used by Rack-based servers to start the application.
-
-require_relative 'config/environment'
-
-if Rails.env.development?
-  map '/git' do
-    run WebGit::Server
-  end
-end
-
-map '/' do
-  run Rails.application
-end
+run Rails.application
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,9 +35,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
@@ -47,19 +44,6 @@ Rails.application.configure do
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
-
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
-
-  # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "rps_css_production"
-
-  config.action_mailer.perform_caching = false
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
